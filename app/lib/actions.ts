35935b2aeda41dc823b980f49708d0945e3cc967
hoapooh <hoapooh2912@gmail.com>
2024-10-22 16:43:30 +0700

    Create and Update Form, Improve SEO and handling errors

---
+++ app/lib/actions.ts
@@ -0,0 +1,135 @@
+"use server"
+
+import { z } from "zod"
+import { sql } from "@vercel/postgres"
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
+
+const FormSchema = z.object({
+	id: z.string(),
+	customerId: z.string({
+		invalid_type_error: "Please select a customer.",
+	}),
+	amount: z.coerce.number().gt(0, { message: "Please enter an amount greater than $0." }),
+	status: z.enum(["pending", "paid"], {
+		invalid_type_error: "Please select an invoice status.",
+	}),
+	date: z.string(),
+})
+
+export type State = {
+	errors?: {
+		customerId?: string[]
+		amount?: string[]
+		status?: string[]
+	}
+	message?: string | null
+}
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
+
+// INFO: By adding the 'use server', you mark all the exported functions within the file as Server Actions.
+// INFO: These server functions can then be imported and used in Client and Server components.
+
+export async function createInvoice(prevState: State, formData: FormData) {
+	// INFO: You can extract specific data here from the FormData object.
+	// const { customerId, amount, status } = CreateInvoice.parse({
+	//   customerId: formData.get('customerId'),
+	//   amount: formData.get('amount'),
+	//   status: formData.get('status'),
+	// });
+
+	// INFO: Or you can convert the FormData object to a plain object.
+	// const { customerId, amount, status } = CreateInvoice.parse(Object.fromEntries(formData))
+
+	// Validate form fields using Zod
+	const validatedFields = CreateInvoice.safeParse({
+		customerId: formData.get("customerId"),
+		amount: formData.get("amount"),
+		status: formData.get("status"),
+	})
+
+	// If form validation fails, return errors early. Otherwise, continue.
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: "Missing Fields. Failed to Create Invoice.",
+		}
+	}
+
+	// Prepare data for insertion into the database
+	const { customerId, amount, status } = validatedFields.data
+	const amountInCents = amount * 100
+	const date = new Date().toISOString().split("T")[0] // INFO: 2024-10-22T00:00:00.000Z --> 2024-10-22
+
+	try {
+		await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `
+	} catch (error) {
+		console.error(error)
+
+		return {
+			message: "Database Error: Failed to Create Invoice.",
+		}
+	}
+
+	revalidatePath("/dashboard/invoices")
+	redirect("/dashboard/invoices")
+}
+
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+	// const { customerId, amount, status } = UpdateInvoice.parse({
+	// 	customerId: formData.get("customerId"),
+	// 	amount: formData.get("amount"),
+	// 	status: formData.get("status"),
+	// })
+
+	// Validate form fields using Zod
+	const validatedFields = UpdateInvoice.safeParse({
+		customerId: formData.get("customerId"),
+		amount: formData.get("amount"),
+		status: formData.get("status"),
+	})
+
+	// If form validation fails, return errors early. Otherwise, continue.
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: "Missing Fields. Failed to Update Invoice.",
+		}
+	}
+
+	// Prepare data for insertion into the database
+	const { customerId, amount, status } = validatedFields.data
+	const amountInCents = amount * 100
+
+	try {
+		await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `
+	} catch (error) {
+		console.error(error)
+
+		return { message: "Database Error: Failed to Update Invoice." }
+	}
+
+	revalidatePath("/dashboard/invoices")
+	redirect("/dashboard/invoices")
+}
+
+export async function deleteInvoice(id: string) {
+	try {
+		await sql`DELETE FROM invoices WHERE id = ${id}`
+		revalidatePath("/dashboard/invoices")
+		return { message: "Deleted Invoice." }
+	} catch (error) {
+		console.error(error)
+
+		return { message: "Database Error: Failed to Delete Invoice." }
+	}
+}
