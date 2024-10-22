5fdc6992b3b68c31f50afe5c6071e078a5945bd8
hoapooh <hoapooh2912@gmail.com>
2024-10-22 19:11:53 +0700

    Finish Nextjs Official course

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-	return <p>Customers Page</p>
+import CustomersTable from "@/app/ui/customers/table"
+import { Metadata } from "next"
+import { fetchCustomersFormatted } from "@/app/lib/data"
+import { Suspense } from "react"
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons"
+
+export const metadata: Metadata = {
+	title: "Customers",
+}
+
+export default async function Page() {
+	const allCustomers = await fetchCustomersFormatted()
+
+	console.log(allCustomers)
+
+	return (
+		<Suspense key={"customer-table"} fallback={<InvoicesTableSkeleton />}>
+			<CustomersTable customers={allCustomers} />
+		</Suspense>
+	)
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,6 +2,7 @@ import { sql } from "@vercel/postgres"
 import {
 	CustomerField,
 	CustomersTableType,
+	FormattedCustomersTable,
 	InvoiceForm,
 	InvoicesTable,
 	LatestInvoiceRaw,
@@ -210,5 +211,30 @@ export async function fetchFilteredCustomers(query: string) {
 	} catch (err) {
 		console.error("Database Error:", err)
 		throw new Error("Failed to fetch customer table.")
+	}
+}
+
+export async function fetchCustomersFormatted() {
+	try {
+		const data = await sql<FormattedCustomersTable>`
+		SELECT
+		  customers.id,
+		  customers.name,
+		  customers.email,
+		  customers.image_url,
+		  COUNT(invoices.id) AS total_invoices,
+		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
+		FROM customers
+		LEFT JOIN invoices ON customers.id = invoices.customer_id
+		GROUP BY customers.id, customers.name, customers.email, customers.image_url
+		ORDER BY customers.name ASC
+    `
+
+		const customers = data.rows
+		return customers
+	} catch (err) {
+		console.error("Database Error:", err)
+		throw new Error("Failed to fetch all customers.")
 	}
 }
